b9724ea8629dea3c6048fd1a83bd2a3e53a86ffc
Anikesh Chouhan <anikeshmaheshwari558@gmail.com>
2023-09-12 03:11:23 +0530

    Init command

---
+++ git.java
@@ -0,0 +1,26 @@
+package GitDemo;
+
+public class git {
+
+  public static void main(String[] args) {
+    // System.out.println("in the git we are basicllay used commannd line
+    // like
+    // git clone Or git init
+    // git status
+    // git add Or git add . or git add -A
+    // git commit -m "messeage"
+    // git push origin main/master");
+    System.out.println(
+      "System.out.println(\"in the git we are basicllay used commannd line"
+    );
+    System.out.println(
+      "like \r\n" +
+      "    git clone Or git init\r\n" +
+      "     git status\r\n" +
+      "    git add Or git add . or git add -A"
+    );
+    System.out.println(
+      "git commit -m \"messeage\"" + "   git push origin main/master"
+    );
+  }
+}
---
+++ git.md
@@ -0,0 +1,3 @@
+Auther -Anikesh Chouhan
+Date - 12/09/2023
+project name - Git full information
